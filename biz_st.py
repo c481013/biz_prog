3497a1fc698ef05df40b329f6783721df9188912
c481013 <c481013@g.hongik.ac.kr>
2026-05-26 16:02:31 +0900

    Update biz_st.py

--- biz_st.py
+++ biz_st.py
@@ -1,7 +1,28 @@
-import streamlit as st # streamlit 라이브러리 임포트
+import streamlit as st 
 
-# 타이틀 텍스트 출력
-st.title("첫번째 웹 어플 만들기")
+"""
+# 비지니스 모델 분석
 
-"## 이건 부제목"
+[네이버](http://www.naver.com)  
+[홍익대학교](http://hongik.ac.kr)
+
+이것이 일반 본문 **이것이 굵은 글씨** *이것이 기울임 글씨* ~~이것이 취소선~~  
+:red[빨간색 글씨]
+
+```python
+impotr streamlit as st
+
+print("코드 블록")
+
+"""
+
+st.caption('캡션(작고 흐린 글씨로 표현됨):st.caption()')
+
+with st.echo():
+    # 이 블록의 코드와 결과를 출력
+    name="Daeun Kim"
+    st.write("Hello, Streamlit!",name)
+
+st.latex('\int_a^b f(x)dx')   
+"$$\int_a^b f(x)dx$$"
 
