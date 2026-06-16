52fc5586dd99a5362731e5781e86b47bbd1df2aa
c481013 <c481013@g.hongik.ac.kr>
2026-06-16 11:57:48 +0900

    Update biz_final_st.py

--- biz_final_st.py
+++ biz_final_st.py
@@ -1,17 +1,1 @@
-import streamlit as st 
 
-"""
-# 비지니스 모델 분석
-
-[네이버](http://www.naver.com)  
-[홍익대학교](http://hongik.ac.kr)
-
-이것이 일반 본문 **이것이 굵은 글씨** *이것이 기울임 글씨* ~~이것이 취소선~~  
-:red[빨간색 글씨]
-
-```python
-impotr streamlit as st
-
-print("코드 블록")
-
-"""
